2a51beb9c4768563a74d38eca3ea015f1c886748
DerMeister <v.savchenko2408@gmail.com>
2025-03-07 06:50:42 +0200

    move()

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
-#include <cstdlib>
+//#include <cstdlib>
 #include "Player.h"
 #include "Field.h"
 
+
+
 int main()
 {
+    Player p;
     Field fl;
     fl.drawField();
+    char move;
+    while(move != '0')
+    {
+        std::cin >> move;
+        switch(move)
+        {
+            case 'w':
 
+            case 's':
+
+            case 'a':
+
+            case 'd':
+
+            default:
+            std::cerr << "Wrong input!" << std::endl;
+        }
+    }
 #ifdef linux
     std::cout << "\t\t\tExit program!" << std::endl;
 #endif
